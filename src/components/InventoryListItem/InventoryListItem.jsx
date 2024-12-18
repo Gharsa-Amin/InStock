32f95b97d4261eea04c290f7a7365ce1d02fdfb0
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2024-12-18 10:30:07 -0500

    added styling to button

--- src/components/InventoryListItem/InventoryListItem.jsx
+++ src/components/InventoryListItem/InventoryListItem.jsx
@@ -1,5 +1,13 @@
 import "./InventoryListItem.scss";
 
 const InventoryListItem = () => {
-  return;
+  return (
+    <section className="inventory-list-item">
+      <div className="inventory-list-item__flex-left"></div>
+      <div className="inventory-list-item__flex-right"></div>
+      {/* how do i hide the header component? */}
+    </section>
+  );
 };
+
+export default InventoryListItem;
