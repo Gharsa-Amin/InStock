f048e9ae45054684fbfcee822c49215329da50f1
Vanessa Tseng <vanessatsengyh@gmail.com>
2024-12-17 23:11:59 -0500

    created Header component & updated variables/mixins

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ".App.scss";
-import "./App.css";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* MAKE NOT FOUND PAGE */}
+      </Routes>
     </BrowserRouter>
   );
+  s;
 }
 
 export default App;
