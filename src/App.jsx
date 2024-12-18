444a1bb89cc691809d1f7b100c931f7a08514974
Vanessa Tseng <vanessatsengyh@gmail.com>
2024-12-18 10:47:27 -0500

    created footer component & edited header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         {/* MAKE NOT FOUND PAGE */}
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
