897baba8494ae02ed729914cefbb4dc45020ed5b
Vanessa Tseng <vanessatsengyh@gmail.com>
2024-12-18 21:09:41 -0500

    completed full styling addNewWarehouse form

--- src/components/AddNewWarehouse/AddNewWarehouse.jsx
+++ src/components/AddNewWarehouse/AddNewWarehouse.jsx
@@ -1,12 +1,74 @@
+import { useState } from "react";
 import "./AddNewWarehouse.scss";
 
 export default function AddNewWarehouse() {
+  const [warehouseName, setWarehouseName] = useState("");
+  const [streetAddress, setStreetAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [warehouseNameError, setWarehouseNameError] = useState(false);
+  const [streetAddressError, setStreetAddressError] = useState(false);
+  const [cityError, setCityError] = useState(false);
+  const [countryError, setCountryError] = useState(false);
+  const [contactNameError, setContactNameError] = useState(false);
+  const [positionError, setPositionError] = useState(false);
+  const [phoneNumberError, setPhoneNumberError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    setWarehouseNameError(!warehouseName);
+    setStreetAddressError(!streetAddress);
+    setCityError(!city);
+    setCountryError(!country);
+    setContactNameError(!contactName);
+    setPositionError(!position);
+    setPhoneNumberError(!phoneNumber);
+    setEmailError(!email);
+
+    if (
+      !warehouseName ||
+      !streetAddress ||
+      !city ||
+      !country ||
+      !contactName ||
+      !position ||
+      !phoneNumber ||
+      !email
+    ) {
+      return;
+    }
+  };
+
+  const newWarehouseInfo = {
+    warehouseName,
+    streetAddress,
+    city,
+    country,
+    contactName,
+    position,
+    phoneNumber,
+    email,
+  };
+  //   try {
+  //     const saveformInfo = await axios.post(`${API_URL}/warehouses`, newWarehouseInfo);
+  //   } catch(error){
+  //     console.error(error);
+  //   }
+
   return (
     <div className="new-warehouse">
       <div className="new-warehouse__wrapper">
         <div className="header-wrapper">
           <h1 className="h1">
             <svg
+              className="arrow-back"
               width="24"
               height="24"
               viewBox="0 0 24 24"
@@ -21,7 +83,7 @@ export default function AddNewWarehouse() {
             Add New Warehouse
           </h1>
         </div>
-        <form className="new-warehouse__form">
+        <form onSubmit={handleSubmit} className="new-warehouse__form">
           <div className="new-warehouse__form-wrapper">
             <div className="details details--warehouse">
               <h2 className="warehouse__header">Warehouse Details</h2>
@@ -29,33 +91,133 @@ export default function AddNewWarehouse() {
                 <label className="warehouse__label">Warehouse Name</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="warehouseName"
                   placeholder="Warehouse Name"
+                  value={warehouseName}
+                  className={`warehouse__input ${
+                    warehouseNameError ? "error" : ""
+                  } `}
+                  onChange={(event) => {
+                    setWarehouseName(event.target.value);
+                    setWarehouseNameError(false);
+                  }}
                 />
+                {warehouseNameError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">Street Address</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="streetAddress"
                   placeholder="Street Address"
+                  value={streetAddress}
+                  className={`warehouse__input ${
+                    streetAddressError ? "error" : ""
+                  } `}
+                  onChange={(event) => {
+                    setStreetAddress(event.target.value);
+                    setStreetAddressError(false);
+                  }}
                 />
+                {streetAddressError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">City</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="city"
                   placeholder="City"
+                  value={city}
+                  className={`warehouse__input ${cityError ? "error" : ""} `}
+                  onChange={(event) => {
+                    setCity(event.target.value);
+                    setCityError(false);
+                  }}
                 />
+                {cityError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">Country</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="country"
                   placeholder="Country"
+                  value={country}
+                  className={`warehouse__input ${countryError ? "error" : ""} `}
+                  onChange={(event) => {
+                    setCountry(event.target.value);
+                    setCountryError(false);
+                  }}
                 />
+                {countryError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
             </div>
             <div className="details">
@@ -64,40 +226,142 @@ export default function AddNewWarehouse() {
                 <label className="warehouse__label">Contact Name</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="contactName"
                   placeholder="Contact Name"
+                  value={contactName}
+                  className={`warehouse__input ${
+                    contactNameError ? "error" : ""
+                  } `}
+                  onChange={(event) => {
+                    setContactName(event.target.value);
+                    setContactNameError(false);
+                  }}
                 />
+                {contactNameError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">Position</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="position"
                   placeholder="Position"
+                  value={position}
+                  className={`warehouse__input ${
+                    positionError ? "error" : ""
+                  } `}
+                  onChange={(event) => {
+                    setPosition(event.target.value);
+                    setPositionError(false);
+                  }}
                 />
+                {positionError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">Phone Number</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="phoneNumber"
                   placeholder="Phone Number"
+                  value={phoneNumber}
+                  className={`warehouse__input ${
+                    phoneNumberError ? "error" : ""
+                  } `}
+                  onChange={(event) => {
+                    setPhoneNumber(event.target.value);
+                    setPhoneNumberError(false);
+                  }}
                 />
+                {phoneNumberError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
               <div className="warehouse__info-wrapper">
                 <label className="warehouse__label">Email</label>
                 <input
                   type="text"
-                  className="warehouse__input"
+                  name="email"
                   placeholder="Email"
+                  value={email}
+                  className={`warehouse__input ${emailError ? "error" : ""} `}
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                    setEmailError(false);
+                  }}
                 />
+                {emailError && (
+                  <p className="error-message">
+                    <svg
+                      className="error-svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM13 17H11V15H13V17ZM13 13H11V7H13V13Z"
+                        fill="#C94515"
+                      />
+                    </svg>
+                    This field is required
+                  </p>
+                )}
               </div>
             </div>
           </div>
           <div className="buttons">
             <div className="buttons__wrapper">
               <button className="buttons__button cancel">Cancel</button>
-              <button className="buttons__button add-warehouse">
+              <button type="submit" className="buttons__button add-warehouse">
                 + Add Warehouse
               </button>
             </div>
