373a75051b45f8f605e1577dd0da617674b8e7dd
Gharsa Amin <ghars0718@gmail.com>
2024-12-20 12:53:51 -0500

    added categories

--- src/pages/InventoryPage/IventoryPage.jsx
+++ src/pages/InventoryPage/IventoryPage.jsx
@@ -1,9 +1,9 @@
 import InventoryList from "../../components/InventoryList/InventoryList";
 
 export default function InventoryPage() {
-  return (
-    <>
-      <InventoryList />
-    </>
-  );
+	return (
+		<>
+			<InventoryList />
+		</>
+	);
 }
