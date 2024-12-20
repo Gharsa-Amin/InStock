7ca301a94ca66aff3135e662d21bdd6ed59d8500
Femzodu <oduwoleoluwafemi@gmail.com>
2024-12-20 00:23:43 -0700

    styling corrections

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import "./App.scss";
 import Header from "./components/Header/Header";
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 import HomePage from "./pages/HomePage/HomePage";
-<<<<<<< HEAD
 import WarehouseInventoryList from "./components/WarehouseInventory/WarehouseInventory";
 
-=======
->>>>>>> develop
 import AddNewWarehouse from "./components/AddNewWarehouse/AddNewWarehouse";
 import InventoryList from "./components/InventoryList/InventoryList";
 import Footer from "./components/Footer/Footer";
@@ -20,32 +17,24 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-<<<<<<< HEAD
-      {/* <AddNewWarehouse /> */}
-=======
       <InventoryHeader />
       <WarehouseList />
       {/* <AddNewWarehouse /> */}
       <AddNewInventory />
->>>>>>> develop
       <Routes>
         <Route
           path="/inventory/:inventoryId"
           element={<InventoryDetails />}
         ></Route>
         {/* <Route path="/" element={<HomePage />} /> */}
-<<<<<<< HEAD
         <Route path="/warehouses/:id" element={<WarehouseInventoryList />} />
         {/* MAKE NOT FOUND PAGE */}
-      </Routes>
 
-      {/* <InventoryList /> */}
-=======
+        {/* <InventoryList /> */}
         {/* <Route path="/warehouses/:id" element={<WarehouseDetails />} /> */}
         {/* MAKE NOT FOUND PAGE */}
       </Routes>
       <InventoryList />
->>>>>>> develop
       <Footer />
     </BrowserRouter>
   );
