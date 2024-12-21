e4756491dbc7144cab0f6c2e3bbb3f99ab701a5d
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2024-12-20 23:22:50 -0500

    add new inventory component import

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,32 @@ import Footer from "./components/Footer/Footer";
 import InventoryDetails from "./components/InventoryDetails/InventoryDetails";
 import InventoryPage from "./pages/InventoryPage/IventoryPage";
 import WarehouseInventory from "./components/WarehouseInventory/WarehouseInventory";
+import AddNewInventory from "./components/AddNewInventory/AddNewInventory";
 
 /* MAKE NOT FOUND PAGE */
 import WarehouseEdit from "./components/WarehouseEdit/WarehouseEdit";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Routes>
-				<Route path="/" element={<WarehousePage />}></Route>
-				<Route path="/inventory" element={<InventoryPage />}></Route>
-				<Route
-					path="/inventory/:inventoryId"
-					element={<InventoryDetails />}
-				></Route>
-				<Route path="/addWarehouses" element={<AddNewWarehouse />}></Route>
-				<Route path="/warehouses/:id" element={<WarehouseDetailsPage />} />
-				<Route path="/addinventories" element={<AddNewInventory />}></Route>
-				<Route path="/editWarehouses/:id" element={<WarehouseEdit />}></Route>
-				<Route path="*" element={<NotFoundPage />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
-	);
-
-
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<WarehousePage />}></Route>
+        <Route path="/inventory" element={<InventoryPage />}></Route>
+        <Route
+          path="/inventory/:inventoryId"
+          element={<InventoryDetails />}
+        ></Route>
+        <Route path="/addWarehouses" element={<AddNewWarehouse />}></Route>
+        <Route path="/warehouses/:id" element={<WarehouseDetailsPage />} />
+        <Route path="/addinventories" element={<AddNewInventory />}></Route>
+        <Route path="/editWarehouses/:id" element={<WarehouseEdit />}></Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
