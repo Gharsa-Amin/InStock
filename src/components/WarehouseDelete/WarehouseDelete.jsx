cc1476a086c40de57dd9f12fed09caad4ab079b5
Gharsa Amin <ghars0718@gmail.com>
2024-12-21 01:23:05 -0500

    added styling to the delete button, and used position absolute

--- src/components/WarehouseDelete/WarehouseDelete.jsx
+++ src/components/WarehouseDelete/WarehouseDelete.jsx
@@ -1,49 +1,76 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 export default function WarehouseDelete({
-  closeModal,
-  isModalOpen,
-  setisModalOpen,
-  warehouse,
-  setWarehouses,
+	closeModal,
+	isModalOpen,
+	setisModalOpen,
+	warehouse,
+	setWarehouses,
 }) {
-  const handleDelete = async () => {
-    try {
-      console.log("test");
-      const URL = `http://localhost:8080/api/warehouses/${warehouse.id}`;
-      await axios.delete(URL);
-      setisModalOpen(false);
-    } catch (error) {
-      console.error("Error deleting warehouse:", error);
-    }
-    const fetchWarehouses = async () => {
-      const URL = "http://localhost:8080/api/warehouses";
-      try {
-        const response = await axios.get(URL);
-        console.log(response.data);
-        setWarehouses(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchWarehouses();
-  };
-  const handleCancel = () => {
-    setisModalOpen(false);
-  };
-  return (
-    <section>
-      <h1>Warehouses</h1>
-      <div className="modal">
-        <div className="modal-content">
-          <h2>
-            Please confirm that you would like to delete the from the list of
-            warehouses. You won't be able to undo this action.
-          </h2>
-          <button onClick={() => handleDelete()}>Yes, delete</button>
-          <button onClick={closeModal}>Cancel</button>
-        </div>
-      </div>
-    </section>
-  );
+	const handleDelete = async () => {
+		try {
+			console.log("test");
+			const URL = `http://localhost:8080/api/warehouses/${warehouse.id}`;
+			await axios.delete(URL);
+			setisModalOpen(false);
+		} catch (error) {
+			console.error("Error deleting warehouse:", error);
+		}
+		const fetchWarehouses = async () => {
+			const URL = "http://localhost:8080/api/warehouses";
+			try {
+				const response = await axios.get(URL);
+				console.log(response.data);
+				setWarehouses(response.data);
+			} catch (error) {
+				console.error(error);
+			}
+		};
+		fetchWarehouses();
+	};
+	const handleCancel = () => {
+		setisModalOpen(false);
+	};
+
+	return (
+		<div className="modal-backdrop">
+			<div className="modal">
+				<div className="modal__close">
+					<svg
+						onClick={handleDelete}
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+							fill="#13182C"
+						/>
+					</svg>
+				</div>
+				<div className="modal__content">
+					<div className="modal__content-wrapper">
+						<h1 className="modal__header">Delete warehouse item?</h1>
+						<p className="modal__text">
+							Please confirm that you would like to delete warehouse from the
+							inventory list. You won't be able to undo this action.
+						</p>
+					</div>
+					<div className="modal-buttons">
+						<button
+							onClick={() => handleDelete()}
+							className="modal-buttons__cancel"
+						>
+							Yes, delete
+						</button>
+						<button onClick={closeModal} className="modal-buttons__delete">
+							Cancel
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/components/WarehouseShared/WarehouseShared.jsx
+++ src/components/WarehouseShared/WarehouseShared.jsx
@@ -1,15 +1,15 @@
 import WarehouseList from "../WarehouseList/WarehouseList";
 import WarehouseListHeader from "../WarehouseListHeader/WarehouseListHeader";
 import WarehouseNav from "../WarehouseNav/WarehouseNav";
-
+import "./WarehouseShared.scss";
 export default function WarehouseShared() {
 	return (
 		<section className="warehouse-page">
-			<div className="child">
+			<section className="child">
 				<WarehouseListHeader />
 				<WarehouseNav />
 				<WarehouseList />
-			</div>
+			</section>
 		</section>
 	);
 }
