aae5ab2b75d95af96fa9f73be491c57f29ad04ba
Sadia Bahadoor <sadia.bahadoor@gmail.com>
2024-12-20 23:08:45 -0500

    added routing

--- src/components/WarehouseDelete/WarehouseDelete.jsx
+++ src/components/WarehouseDelete/WarehouseDelete.jsx
@@ -1,53 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-
 export default function WarehouseDelete({
-	closeModal,
-	isModalOpen,
-	setisModalOpen,
-	warehouse,
-	setWarehouses,
+  closeModal,
+  isModalOpen,
+  setisModalOpen,
+  warehouse,
+  setWarehouses,
 }) {
-	const handleDelete = async () => {
-		try {
-			console.log("test");
-			const URL = `http://localhost:8080/api/warehouses/${warehouse.id}`;
-			await axios.delete(URL);
-			setisModalOpen(false);
-		} catch (error) {
-			console.error("Error deleting warehouse:", error);
-		}
-		const fetchWarehouses = async () => {
-			const URL = "http://localhost:8080/api/warehouses";
-			try {
-				const response = await axios.get(URL);
-				console.log(response.data);
-
-				setWarehouses(response.data);
-			} catch (error) {
-				console.error(error);
-			}
-		};
-		fetchWarehouses();
-	};
-
-	const handleCancel = () => {
-		setisModalOpen(false);
-	};
-
-	return (
-		<section>
-			<h1>Warehouses</h1>
-			<div className="modal">
-				<div className="modal-content">
-					<h2>
-						Please confirm that you would like to delete the from the list of
-						warehouses. You won't be able to undo this action.
-					</h2>
-					<button onClick={() => handleDelete()}>Yes, delete</button>
-					<button onClick={closeModal}>Cancel</button>
-				</div>
-			</div>
-		</section>
-	);
+  const handleDelete = async () => {
+    try {
+      console.log("test");
+      const URL = `http://localhost:8080/api/warehouses/${warehouse.id}`;
+      await axios.delete(URL);
+      setisModalOpen(false);
+    } catch (error) {
+      console.error("Error deleting warehouse:", error);
+    }
+    const fetchWarehouses = async () => {
+      const URL = "http://localhost:8080/api/warehouses";
+      try {
+        const response = await axios.get(URL);
+        console.log(response.data);
+        setWarehouses(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchWarehouses();
+  };
+  const handleCancel = () => {
+    setisModalOpen(false);
+  };
+  return (
+    <section>
+      <h1>Warehouses</h1>
+      <div className="modal">
+        <div className="modal-content">
+          <h2>
+            Please confirm that you would like to delete the from the list of
+            warehouses. You won't be able to undo this action.
+          </h2>
+          <button onClick={() => handleDelete()}>Yes, delete</button>
+          <button onClick={closeModal}>Cancel</button>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -7,10 +7,9 @@ import chevron from "../../assets/icons/chevron_right-24px.svg";
 import remove from "../../assets/icons/delete_outline-24px.svg";
 import edit from "../../assets/icons/edit-24px.svg";
 import WarehouseListHeader from "../WarehouseListHeader/WarehouseListHeader";
-import SortSVG from "../SortSVG/SortSVG";
 import WarehouseDelete from "../WarehouseDelete/WarehouseDelete";
 
-export default function WarehouseList({}) {
+export default function WarehouseList() {
   const [warehouses, setWarehouses] = useState([]);
   const [selectedWarehouse, setSelectedWarehouse] = useState(null);
   const [isModalOpen, setisModalOpen] = useState(false);
@@ -18,7 +17,6 @@ export default function WarehouseList({}) {
     setSelectedWarehouse(warehouse);
     setisModalOpen(true);
   };
-
   const closeModal = () => {
     setSelectedWarehouse(null);
     setisModalOpen(false);
@@ -29,7 +27,6 @@ export default function WarehouseList({}) {
       try {
         const response = await axios.get(URL);
         console.log(response.data);
-
         setWarehouses(response.data);
       } catch (error) {
         console.error(error);
@@ -37,87 +34,9 @@ export default function WarehouseList({}) {
     };
     fetchWarehouses();
   }, []);
-
   return (
     <>
       <section className="warehouse-list">
-        <div className="warehouse-top__row">
-          <div className="warehouse-top__row-title">
-            <h3>WAREHOUSE</h3>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                fill="#5C667E"
-              />
-            </svg>
-          </div>
-          <div className="warehouse-top__row-title">
-            <h3>ADDRESS</h3>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                fill="#5C667E"
-              />
-            </svg>
-          </div>
-          <div className="warehouse-top__row-title">
-            <h3>CONTACT NAME</h3>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                fill="#5C667E"
-              />
-            </svg>
-          </div>
-          <div className="warehouse-top__row-title">
-            <h3>CONTACT INFORMATION</h3>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                fill="#5C667E"
-              />
-            </svg>
-          </div>
-          <div className="warehouse-top__row-title">
-            <h3>ACTIONS</h3>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 5.83L15.17 9L16.58 7.59L12 3L7.41003 7.59L8.83003 9L12 5.83ZM12 18.17L8.83003 15L7.42003 16.41L12 21L16.59 16.41L15.17 15L12 18.17Z"
-                fill="#5C667E"
-              />
-            </svg>
-          </div>
-        </div>
         {warehouses.map((warehouse) => (
           <div key={warehouses.id}>
             <div className="top-tablet">
@@ -161,20 +80,18 @@ export default function WarehouseList({}) {
                   <div className="warehouse-list__email">
                     {warehouse.contact_email}
                   </div>
-                  <Link to={`/editWarehouses/${warehouse.id}`}>
-                    <img
-                      className="warehouse-list__image-contact"
-                      src={edit}
-                      alt=""
-                    />
-                  </Link>
+                  <img
+                    className="warehouse-list__image-contact"
+                    src={edit}
+                    alt=""
+                  />
                 </div>
               </div>
               <div className="wrapper-item">
                 <div className="warehouse-actions">
                   <img
                     className="warehouse-list__image-tablet"
-                    onClick={openModal}
+                    onClick={() => openModal(warehouse)}
                     src={remove}
                   />
                   <Link to={`/editWarehouses/${warehouse.id}`}>
@@ -188,14 +105,16 @@ export default function WarehouseList({}) {
               </div>
             </div>
           </div>
+          // </div>
         ))}
       </section>
-
       {isModalOpen ? (
         <WarehouseDelete
+          setWarehouses={setWarehouses}
           closeModal={closeModal}
           isModalOpen={isModalOpen}
           setisModalOpen={setisModalOpen}
+          warehouse={selectedWarehouse}
         />
       ) : (
         ""
